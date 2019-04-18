e04851e5271fae8c0dba77e0822f572d7ea4c536
HarryHoudini <englishmanrt@gmail.com>
2019-04-18 23:57:24 +0300

    filter not work

--- simpleProject/suite/__tia-tests__/00_helpers/01_addNewFabric.tia.js
+++ simpleProject/suite/__tia-tests__/00_helpers/01_addNewFabric.tia.js
@@ -20,32 +20,44 @@ async function test() {
   await eC.button.a.click(
     '#assets &tabsContainer > button[text="Организация"]'
   );
-  await eC.textfield.a.sendKeys(
-    "#organization &form > textarea[name=name]",
-    "Next Generation"
-  );
-  await eC.combobox.a.setByMouse(
-    "#organization &form > combobox[name=type_id]",
-    "Организация"
-  );
-  await eC.combobox.a.setByMouse(
-    "#organization &form > tagfield[name=cities_id]",
-    "Воронеж"
-  );
-  await s.driver.sleep(1000);
-  await eC.combobox.a.setByMouse(
-    "#organization &form > usertagfield[name=admin_ids]",
-    "Иван Петров (ipetrov@rvlab.net)"
-  );
-  await eC.combobox.a.setByMouse(
-    "#organization &compliance_manager",
-    "Иван Петров (ipetrov@rvlab.net)"
-  );
-  await eC.textfield.a.sendKeys(
-    "#organization &form > textarea[name=description]",
-    "Powerful and Biggest Corporation"
-  );
-  await eC.button.a.click('#organization &form > button[text="Добавить"]');
+  // await eC.textfield.a.sendKeys(
+  //   "#organization &form > textarea[name=name]",
+  //   "Next Generation"
+  // );
+  // await eC.combobox.a.setByMouse(
+  //   "#organization &form > combobox[name=type_id]",
+  //   "Организация"
+  // );
+  // await eC.combobox.a.setByMouse(
+  //   "#organization &form > tagfield[name=cities_id]",
+  //   "Воронеж"
+  // );
+  // await s.driver.sleep(1000);
+  // await eC.combobox.a.setByMouse(
+  //   "#organization &form > usertagfield[name=admin_ids]",
+  //   "Иван Петров (ipetrov@rvlab.net)"
+  // );
+  // await eC.combobox.a.setByMouse(
+  //   "#organization &compliance_manager",
+  //   "Иван Петров (ipetrov@rvlab.net)"
+  // );
+  // await eC.textfield.a.sendKeys(
+  //   "#organization &form > textarea[name=description]",
+  //   "Powerful and Biggest Corporation"
+  // );
+  // await eC.button.a.click('#organization &form > button[text="Добавить"]');
+  await gT.eC.gridcolumn.a.clickTrigger('#organization &grid > headercontainer > multiline_treecolumn[text="Наименование"]')
+  await s.driver.sleep(2000)
+  await gT.eC.component.a.click('menu > menucheckitem#filters')
+  await s.driver.sleep(2000)
+  // await eC.textfield.a.clickInput('menu > textfield')
+  await eC.textfield.a.click('menu > textfield')
+  await eC.textfield.a.sendKeys('menu > textfield', 'Next')
+  await eC.textfield.a.sendKeys('menu > textfield', 'Next')
+  await s.driver.sleep(11000)
+
+  await gT.e.explore.init();  
+
   await s.driver.quit();
 }
 
