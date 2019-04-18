a9a8974b528040275518148bfb576519e39a6fa6
HarryHoudini <englishmanrt@gmail.com>
2019-04-18 12:38:52 +0300

    add exjs initialisation

--- simpleProject/suite/__tia-tests__/00_helpers/01_addNewFabric.tia.js
+++ simpleProject/suite/__tia-tests__/00_helpers/01_addNewFabric.tia.js
@@ -1,20 +1,32 @@
 const { t, l, s } = gT;
+
 const eC = gT.eC;
 
 async function test() {
   t.setTitle("Add new fabric");
-  await s.driver.init(false, true);
+  await s.driver.init();
   await s.browser.maximize(true);
-  await s.browser.loadPage("https://test5-demo.rvision.pro/login", true);
-  await s.driver.sleep(3000, true);
+  await s.browser.loadPage("https://test5-demo.rvision.pro/login");
   await s.wait.waitForElementEnabledAndVisibleById("username", 10000);
-   await s.browser.executeScript(
-    'var usr = document.querySelector("#username");var psw = document.querySelector("#password"); usr.value = "admin";psw.value = "admin"'
-  ); //not found tia methods for send value  
-  await s.uA.clickById('submit')
-  await s.driver.sleep(5000, true);
+  await gT.s.uA.sendKeysById('username', 'admin');
+  await gT.s.uA.sendKeysById('password', 'admin'); 
+  await s.uA.clickById("submit");  
+  await s.driver.sleep(10000);
+  await gT.s.browser.initTiaBrHelpers();
+  await gT.e.initTiaExtJsBrHelpers();  
+  // await gT.e.wait.ajaxRequestsFinish(1000)  
+  await gT.eC.tab.a.click('#r-main > tabbar > tab[text="Активы"]')
+  await  gT.eC.button.a.click('#assets &tabsContainer > button[text="Организация"]')
+
+
+  // await gT.s.browser.initTiaBrHelpers();
+  // await gT.e.initTiaExtJsBrHelpers();
+
   await gT.e.explore.init();
-  await s.driver.sleep(10000, true);
-  await s.driver.quit(true);}
+
+  await s.driver.sleep(8000);
+
+  await s.driver.quit();
+}
 
 module.exports = test();
--- simpleProject/suite/__tia-tests__/00_helpers/022_clientExceptions.tia.ts
+++ simpleProject/suite/__tia-tests__/00_helpers/022_clientExceptions.tia.ts
@@ -1,13 +1,15 @@
 import {gT} from "tia"
 const { t, l, s, sOrig } = gT;
 const eC = gT.eC;
+import {gIn} from "tia"
 
 async function test() {
   t.setTitle("Test for client exceptions");
   await s.driver.init(false, true)
   await s.driver.sleep(10000, true)
   await s.driver.sleep(10000, true)
-  gT.e.explore.
+
+  await gT.e.wait.ajaxRequestsFinish(1000)  
   await s.uA.sendKeysById('')
   await s.browser.loadPage("https://test5-demo.rvision.pro/login", true)
   await s.wait.waitForElementEnabledAndVisibleById("username", 10000);
@@ -18,6 +20,16 @@ async function test() {
   await s.driver.sleep(5000, true);
   await s.uA.sendEnterById('submit')
 
+  await gT.eC.tab.a.click('#r-main > tabbar > tab[text=l"protective_system"]')
+
+  await gT.s.browser.initTiaBrHelpers();
+  await gT.e.initTiaExtJsBrHelpers();
+  await  gT.eC.button.a.click
+  ('#assets &tabsContainer > button[text="Организация"]')
+  await gT.eC.textfield.a.sendKeys
+('assets-organization-form > textarea[name=name]')
+('#organization &form > textarea[name=name]', '')
+  
   // l.println('No exceptions and console logs:');
   // await s.browser.printCaughtExceptions(true);
   // await s.browser.printSelBrowserLogs();
